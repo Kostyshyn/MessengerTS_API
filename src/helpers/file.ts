270efeb8f87dfe3ab3c4d1fc1d60433bc07889ff
Kostyshyn Andriy <kostyshynandriy2@gmail.com>
2020-05-05 23:09:52 +0300

    [+] fs helpers[moveFile, deleteFile, checkDir]

---
+++ src/helpers/file.ts
@@ -0,0 +1,58 @@
+import * as fs from 'fs';
+import * as util from 'util';
+import * as path from 'path';
+import * as shell from 'shelljs';
+
+export const moveFile = (oldPath, newPath): Promise<any> => {
+  const from = path.normalize(oldPath);
+  const to = path.normalize(newPath);
+  return new Promise((resolve, reject) => {
+    fs.rename(from, to, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(true);
+    });    
+  });
+};
+
+export const deleteFile = (filePath: string): Promise<any> => {
+  const p = path.normalize(filePath);
+  return new Promise((resolve, reject) => {
+    fs.unlink(p, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(true);
+    }); 
+  });
+};
+
+export const checkDir = (filePath: string): any => {
+  const p = path.normalize(filePath);
+  try {
+    fs.statSync(p);
+  } catch (err) {
+    shell.mkdir('-p', p);
+  }
+};
+
+// export const copyFile = (from, to): Promise<any> => {
+
+// };
+
+//     function copy() {
+//         var readStream = fs.createReadStream(oldPath);
+//         var writeStream = fs.createWriteStream(newPath);
+
+//         readStream.on('error', callback);
+//         writeStream.on('error', callback);
+
+//         readStream.on('close', function () {
+//             fs.unlink(oldPath, callback);
+//         });
+
+//         readStream.pipe(writeStream);
+//     }
